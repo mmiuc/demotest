1a5576703c24fe5b42d8467677654d48db4a3794
Va1kyrie-0218 <67545523+Va1kyrie-0218@users.noreply.github.com>
2025-06-19 14:04:11 +0800

    add gitingore

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ app.use((req, res, next) => {
 const data = [
     { id: 1, name: '张三', age: 25 },
     { id: 2, name: '李四', age: 30 },
-    { id: 3, name: '王五', age: 35 },
-    { id: 4, name: 'cc', age: 20 },
-    { id: 5, name: 'mm', age: 22 },
-    { id: 6, name: 'aa', age: 24 }
+    { id: 3, name: '王五', age: 35 }
 ];
 
 app.get('/data', (req, res) => {
